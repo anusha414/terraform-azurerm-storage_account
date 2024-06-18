8d97fe0d23b87d9d4602a5f6994d71a58fc15f09
anusha414 <53256022+anusha414@users.noreply.github.com>
2024-06-18 21:49:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "storage_account" {
-  source  = "https://registry.terraform.io/modules/data-platform-hq/storage-account/azurerm"
+  source  = "data-platform-hq/storage-account/azurerm"
   version = "1.5.4"
   project                         = "datahq"
   env                             = "example"
   location                        = "eastus"
   resource_group                  = "example_rg"
 }
-output "storage_id" {
+output "storage_account_id" {
   description= "the id of storage account is:"
-  value = "${module.storage_account.id}"
+  value = "${module.storage_account.storage_account_id}"
 }
